31513575c60063f54a8806a14b9ad46d61e3cde0
Matthew-Salaway <matthewsalaway@gmail.com>
2023-04-12 15:32:25 -0700

    sign up backend complete

--- pages/login.js
+++ pages/login.js
@@ -12,7 +12,7 @@ export default function Login() {
     <div>
       <NavBar />
       <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-        <h1 className="font-bold mb-8 text-3xl text-center">Log in</h1>
+        <h1 className="font-bold mb-8 text-3xl text-center mt-16">Log in</h1>
         <input
           className="block border border-grey-light w-full p-3 rounded mb-4"
           name="username"
@@ -23,7 +23,7 @@ export default function Login() {
           name="password"
           placeholder="Password"
         ></input>
-        <h4 className="font-bold">Forgot Password?</h4>
+        {/* <h4 className="font-bold">Forgot Password?</h4> */}
         <br></br>
         <button
           className="mx-10 cursor-pointer bg-green-600 text-white  px-4 py-2 hover:bg-green-700 rounded-full  text-lg font-medium lg:text-xl lg:px-20 "

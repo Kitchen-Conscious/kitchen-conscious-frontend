cbdb2866c5772fbd31ecefb6815510d1ab524c62
Matthew-Salaway <matthewsalaway@gmail.com>
2023-04-12 14:59:26 -0700

    backend start

--- pages/login.js
+++ pages/login.js
@@ -2,41 +2,49 @@ import MyContext from "../src/myContext";
 import { useContext } from "react";
 import NavBar from "@/src/NavBar";
 
-
 // when the user is signed up, set the state of the userName to the new user's name
 // Then redirect to the main page
 
 export default function Login() {
+  const { userName, updateUserName } = useContext(MyContext);
 
-    const { userName, updateUserName } = useContext(MyContext);
-
-    return (
+  return (
+    <div>
+      <NavBar />
+      <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+        <h1 className="font-bold mb-8 text-3xl text-center">Log in</h1>
+        <input
+          className="block border border-grey-light w-full p-3 rounded mb-4"
+          name="username"
+          placeholder="Username"
+        ></input>
+        <input
+          className="block border border-grey-light w-full p-3 rounded mb-4"
+          name="password"
+          placeholder="Password"
+        ></input>
+        <h4 className="font-bold">Forgot Password?</h4>
+        <br></br>
+        <button
+          className="mx-10 cursor-pointer bg-green-600 text-white  px-4 py-2 hover:bg-green-700 rounded-full  text-lg font-medium lg:text-xl lg:px-20 "
+          onClick={() => {
+            updateUserName("john");
+            window.location.href = "http://localhost:3000/main";
+          }}
+        >
+          Log In
+        </button>
+        <br></br>
         <div>
-            <NavBar />
-            <div className = "container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                <h1 className = "font-bold mb-8 text-3xl text-center">Log in</h1>
-                <input
-                className = "block border border-grey-light w-full p-3 rounded mb-4"
-                name = "username"
-                placeholder = "Username"
-                ></input>
-                <input
-                className = "block border border-grey-light w-full p-3 rounded mb-4"
-                name = "password"
-                placeholder = "Password"
-                ></input>
-                <h4 className = "font-bold" >Forgot Password?</h4>
-                <br></br>
-                <button className="mx-10 cursor-pointer bg-green-600 text-white  px-4 py-2 hover:bg-green-700 rounded-full  text-lg font-medium lg:text-xl lg:px-20 " onClick={() => {updateUserName("john")}}>Log In</button>
-                <br></br>
-                <div>
-                New to KitchenConscious?&nbsp; 
-                <a className = "text-decoration-line: underline text-green-600 font-bold" href="../signup">
-                    Sign-up
-                </a>
-                </div>                                
-            </div>
-
+          New to KitchenConscious?&nbsp;
+          <a
+            className="text-decoration-line: underline text-green-600 font-bold"
+            href="../signup"
+          >
+            Sign-up
+          </a>
         </div>
-    )}
-
+      </div>
+    </div>
+  );
+}
--- pages/signup.js
+++ pages/signup.js
@@ -8,28 +8,58 @@ import Link from "next/link"; // for next js
 // Then redirect to the main page
 
 export default function Signup() {
-  const [inputs, setInputs] = useState({});
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+    email: "",
+    phone: "",
+  });
 
   const handleChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
-    setInputs(values => ({ ...values, [name]: value }))
-  }
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    //POST request to database
-  }
+    // POST request to database to http://localhost:8080/register
+    // include body with username and password
+
+    console.log(inputs.username);
+    console.log(inputs.password);
+
+    fetch("http://localhost:8080/register", {
+      method: "POST",
+      headers: {
+        accept: "*/*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: inputs.username,
+        password: inputs.password,
+      }),
+    }).then((response) => console.log(JSON.stringify(response)));
+
+    //redirect to main page
+
+    //window.location.href = "http://localhost:3000/main";
+  };
 
   return (
     <div>
       <NavBar />
 
-      <label className="flex justify-center py-12 text-3xl font-bold text-gray-600">Sign Up</label>
+      <label className="flex justify-center py-12 text-3xl font-bold text-gray-600">
+        Sign Up
+      </label>
 
       <div className="flex justify-center">
         <br />
-        <form onSubmit={handleSubmit} className="w-full max-w-md flex flex-col items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-md flex flex-col items-center"
+        >
           <input
             type="email"
             id="email"
@@ -37,7 +67,8 @@ export default function Signup() {
             className="rounded-xl  bg-gray-100 border-gray-400 border p-1 w-60"
             value={inputs.email || ""}
             onChange={handleChange}
-            required placeholder=" Email"
+            required
+            placeholder=" Email"
           />
           <br />
 
@@ -48,7 +79,8 @@ export default function Signup() {
             className="rounded-xl  bg-gray-100 border-gray-400 border p-1 w-60"
             value={inputs.phone || ""}
             onChange={handleChange}
-            required placeholder=" Phone Number"
+            required
+            placeholder=" Phone Number"
           />
           <br />
 
@@ -59,10 +91,10 @@ export default function Signup() {
             className="rounded-xl  bg-gray-100 border-gray-400 border p-1 w-60"
             value={inputs.username || ""}
             onChange={handleChange}
-            required placeholder=" Username"
+            required
+            placeholder=" Username"
           />
           <br />
-
 
           <input
             type="password"
@@ -71,18 +103,22 @@ export default function Signup() {
             className="rounded-xl  bg-gray-100 border-gray-400 border p-1 w-60"
             value={inputs.password || ""}
             onChange={handleChange}
-            required placeholder=" Password"
+            required
+            placeholder=" Password"
           />
           <br />
 
-          <button type="submit" className=" w-60 shadow-black-lg mx-10 cursor-pointer bg-green-600/95 text-white  px-4 py-2 hover:bg-green-700 rounded-xl  text-lg font-medium lg:text-xl lg:px-20 ">SIGN UP</button>
-
+          <button
+            type="submit"
+            className=" w-60 shadow-black-lg mx-10 cursor-pointer bg-green-600/95 text-white  px-4 py-2 hover:bg-green-700 rounded-xl  text-lg font-medium lg:text-xl lg:px-20 "
+          >
+            SIGN UP
+          </button>
         </form>
       </div>
 
       <div className="text-center py-4">
         Already have an account?
-
         <Link
           href="../login"
           className="mx-1 cursor-pointer text-green-600  text-md font-medium "
@@ -90,10 +126,6 @@ export default function Signup() {
           Log-In
         </Link>
       </div>
-
     </div>
-
   );
-
-
 }
